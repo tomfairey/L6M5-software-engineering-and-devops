29d282b4abc5c699c86feccb50ed84174d0bf37d
Thomas Fairey <13948838+tomfairey@users.noreply.github.com>
2024-12-11 23:52:22 +0000

    Dockerised development container
    - Docker container to support development with "docker compose up"
    TODO: Add support for 'application' and check 'database' correctly works
    - Add missing npm requirement

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,7 @@ import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export const configObject = {
 	plugins: [react(), legacy()],
 	test: {
 		globals: true,
@@ -25,4 +24,7 @@ export default defineConfig({
 			},
 		},
 	},
-});
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(configObject);
