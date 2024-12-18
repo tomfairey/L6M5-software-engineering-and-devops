fe91e8bac3acf7285400853d4c3589ca753682ca
Thomas Fairey <13948838+tomfairey@users.noreply.github.com>
2024-12-18 19:08:05 +0000

    Clean-up unused imports
    - Removed imports that seem to have incorrectly snuck in via Intellisense

--- application/src/modules/authentication.ts
+++ application/src/modules/authentication.ts
@@ -7,9 +7,8 @@ import {
 import database from '@modules/database';
 import argon2 from 'argon2';
 import {type PoolClient} from 'pg';
-import crypto, {hash} from 'crypto';
+import crypto from 'crypto';
 import {NextFunction, Request, Response} from 'express';
-import {get} from 'http';
 
 declare global {
 	namespace Express {
